b00c558169ae907a9967b89c58b656183f315f98
Chi Yan Tang <26404212+chi-yan@users.noreply.github.com>
2021-11-12 20:40:39 +1100

    Update app.py
    file too big, use dropbox for model.h5

--- app.py
+++ app.py
@@ -2,11 +2,15 @@ import streamlit as st
 import numpy as np
 import cv2
 import keras
+import requests
+import urllib.request
 from tensorflow.keras.models import load_model
 from streamlit_drawable_canvas import st_canvas
 
 characters="零一二三四五六七八九十百千万亿"
 
+url = 'https://www.dropbox.com/s/1o5953yadfn7uxe/model.h5?dl=1'
+urllib.request.urlretrieve(url, 'model.h5')
 model = keras.models.load_model('model.h5')
 
 st.markdown("""
